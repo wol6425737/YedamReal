8fb4359cc3b7d2d6f56228c9e950730f496a9dce
admin <admin@YD5-18>
2022-01-07 17:18:34 +0900

    0107

--- Chapter07/src/com/yedam/java/ch07_08_interfaceQuiz1/DataAccessObject.java
+++ Chapter07/src/com/yedam/java/ch07_08_interfaceQuiz1/DataAccessObject.java
@@ -3,10 +3,10 @@ package com.yedam.java.ch07_08_interfaceQuiz1;
 public interface DataAccessObject {
 	
 	
-	public abstract void select();
-	public abstract void insert();
-	public abstract void update();
-	public abstract void delete();
+	public void select();
+	public void insert();
+	public void update();
+	public void delete();
 	
 
 }
